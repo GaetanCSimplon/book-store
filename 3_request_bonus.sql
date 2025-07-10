0ac6188fc1a7dd4bc63e237d3603b38ae02384db
GaetanCSimplon <gcourtieupro@gmail.com>
2025-07-10 11:40:53 +0200

    Exo 3 bonus à moitié complété

--- 3_request_bonus.sql
+++ 3_request_bonus.sql
@@ -1,22 +1,28 @@
 -- Bonus SQL
 
 -- 1. Trouver les 3 auteurs dont les livres ont généré le plus de chiffre d’affaires total.
-SELECT a.first_name, a.last_name, SUM(b.price) AS ca
+SELECT a.first_name, a.last_name, SUM(cu.total_amount) AS ca
 FROM customer_order cu
 JOIN book b ON cu.book_id = b.book_id
 JOIN author a ON b.author_id = a.author_id
-GROUP BY a.author_id, a.first_name, a.last_name
+GROUP BY a.author_id
 ORDER BY ca DESC LIMIT 3
 
 -- 2. Calculer le total des ventes par thème de livre, classé du plus au moins vendu (en montant).
-SELECT t.theme_name, SUM(cu.order_id) AS ventes_totales
+SELECT t.theme_name, SUM(cu.total_amount) AS ventes_totales
 FROM customer_order cu
+JOIN book b ON cu.book_id = b.book_id
 JOIN theme t ON b.theme_id = t.theme_id
-JOIN book b ON b.theme_id = t.theme_id
 GROUP BY t.theme_name
 ORDER BY ventes_totales DESC
 
 -- 3. Afficher pour chaque mois de l’année 2024 le nombre de commandes passées et le chiffre d’affaires total.
+SELECT cu.order_date, SUM(cu.total_amount) AS chiffre_affaires
+FROM customer_order cu
+JOIN book b ON b.book_id = cu.book_id
+GROUP BY cu.order_date
+ORDER BY cu.order_date ASC
+WHERE cu.order_date IS 
 
 
 -- 4. Pour chaque auteur, calculer la moyenne des notes des livres écrits, y compris les auteurs dont livres n’ont pas encore d’avis (afficher NULL dans ce cas).
